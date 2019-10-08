40fbf1754bdca2673dac16ae35fd03a46e568749
Rob Hoelz <rob@hoelz.ro>
2019-10-07 21:30:35 -0500

    Implement the exporter

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/hoelzro/constant-exporter
 
 go 1.13
+
+require (
+	github.com/prometheus/client_golang v1.1.0
+	gopkg.in/yaml.v2 v2.2.4
+)
---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	yaml "gopkg.in/yaml.v2"
+)
+
+type metric struct {
+	Name   string
+	Help   string
+	Value  float64
+	Labels map[string]string
+}
+
+type config struct {
+	Metrics []metric
+}
+
+func main() {
+	configFilename := "constants.yml"
+
+	f, err := os.Open(configFilename)
+
+	if err != nil {
+		fmt.Printf("Unable to open config file %v for reading: %v\n", configFilename, err)
+		return
+	}
+
+	defer f.Close()
+
+	d := yaml.NewDecoder(f)
+
+	var cfg config
+
+	err = d.Decode(&cfg)
+
+	if err != nil {
+		fmt.Printf("Unable to decode %v as a YAML file: %v\n", configFilename, err)
+	}
+
+	for _, metric := range cfg.Metrics {
+		labelNames := []string{}
+		labelValues := []string{}
+
+		for key, value := range metric.Labels {
+			labelNames = append(labelNames, key)
+			labelValues = append(labelValues, value)
+		}
+
+		gauge := promauto.NewGaugeVec(prometheus.GaugeOpts{
+			Name: metric.Name,
+			Help: metric.Help,
+		}, labelNames)
+
+		gauge.WithLabelValues(labelValues...).Set(metric.Value)
+	}
+
+	http.ListenAndServe(":9001", promhttp.Handler())
+}
